8f7c9acdb42c0b88a353a59eaf19e2a3ee0a5aba
latototita <pearlmartbusinesses@gmail.com>
2022-07-19 04:36:18 -0500

    cart int problem

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -22,20 +22,20 @@ def cart_quantity(product  , cart):
 
 @register.filter(name='price_total')
 def price_total(product  , cart):
-    return int(product.price) * cart_quantity(product , cart)
+    return int(product.price) * int(cart_quantity(product , cart))
 
 @register.filter(name='total_cart_price')
 def total_cart_price(productes , cart):
     sum = 0 ;
     for p in productes:
-        sum += price_total(p , cart)
+        sum += int(price_total(p , cart))
     return sum
 
 @register.filter(name='total_cart_price_grand')
 def total_cart_price_grand(productes , cart):
     sum = 0 ;
     for p in productes:
-        sum += price_total(p , cart)
+        sum += int(price_total(p , cart))
     sum=sum+3000
     return sum
     
