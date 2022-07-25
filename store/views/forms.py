8489b54c0ac1c93801d61d5f1f2782aaaa937c54
latototita <pearlmartbusinesses@gmail.com>
2022-07-25 06:47:04 -0500

    Laban

--- store/views/forms.py
+++ store/views/forms.py
@@ -49,8 +49,7 @@ class PostForm(BaseForm):
         widgets = {
             'content': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'})
         }
-	image=forms.ImageField(required=True)
-
+    image=forms.ImageField(required=True)
 # Your forms
 class AddVendor_UpdateForm(BaseForm):
     class Meta:
