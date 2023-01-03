37120c26fb8608b2f82786719c6a54abebf1ee07
latototita <pearlmartbusinesses@gmail.com>
2023-01-03 15:37:20 -0600

    first commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -100,13 +100,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 '''
 DATABASES = {
     'default': {
@@ -168,6 +171,9 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import django_heroku
+django_heroku.settings(locals())
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #STMP configuration
